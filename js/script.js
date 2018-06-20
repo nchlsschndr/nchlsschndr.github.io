0011c837a0fe2ac5918002aed5c3f79dc8f7525a
Lars Winters-Ohle <admin@Eddies-iMac.localdomain>
2018-06-20 12:27:15 +0200

    Ani Reset

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $('body').click(function() {
-    $('.wrapper, .text, .one, .two').css( "animation-play-state", "running" );
-  });
+  // $('body').click(function() {
+  //   $('.wrapper, .text, .one, .two').css( "animation-play-state", "running" );
+  // });
+  $('body').click(function(){
+    // restart animation
+    $('.wrapper, .text, .one, .two').css( "animation", "none" );
+    setTimeout(function() {
+        $('.wrapper, .text, .one, .two').css( "animation", "" );
+    }, 10);
+});
 });
